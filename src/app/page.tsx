9e6f1174bca2788580844ba5f8002a0e4fe0ac10
Rafnas-p <rafnasrooni@gmail.com>
2025-01-24 10:37:51 +0530

    redux and login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Login from "@/components/auth/login";
-
+import Home from "@/components/home";
 
 export default function Page() {
   return (
     
     <div>
+      <Home/>
     </div>
   );
 }
