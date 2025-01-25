287027c247cbb75ecfda20d55de5b941aaa23a17
shahid <ms4811879@gmail.com>
2025-01-25 13:16:27 +0530

    middleware

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,2 +1,27 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
+
+const isAdminRoute=(route:string)=>{
+    
+    route.startsWith('/admin')&&route !=='/login'
+}
+
+const isuserProtectedRoute=(route:string)=>{
+    route.startsWith('/user') &&route !=='/login'
+}
+
+const isDoctorProtectedRoute=(route:string)=>{
+    route.startsWith('/doctor') &&route !=='/login'
+}
+
+export function middleware(req:NextRequest){
+    const userType=req.cookies.get('user')?.value
+    const url=req.nextUrl.clone()
+    const pathName=url.pathname
+
+    if(userType !=='admin'&&isAdminRoute(pathName)){
+        url.pathname('/404')
+        return NextResponse.redirect(url)
+    }
+}
