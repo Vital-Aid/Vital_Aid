77243cf430c1c28a36eb44f2e8be3d514e73e83f
shahid <ms4811879@gmail.com>
2025-01-30 18:14:31 +0530

    equipment completed

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,33 +1,4 @@
-// import axios from "axios";
-// import Cookies from 'js-cookie';
 
-// const axiosInstance = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_API_URL,
-//     headers: {
-//       "Content-Type": "application/json",
-      
-//     },
-//   });
-
-//   axiosInstance.interceptors.request.use(
-//     (config) => {
-//       const userCookie = Cookies.get("accessToken");
-//       if (userCookie) {
-//         const user = JSON.parse(userCookie);
-        
-        
-//         if (user) {
-//           config.headers.Authorization = `Bearer ${userCookie}`;
-//         }
-//       }
-//       return config;
-//     },
-//     (error) => {
-//       return Promise.reject(error);
-//     }
-//   );
-  
-//   export default axiosInstance;
 import axios from "axios";
 import Cookies from 'js-cookie';
 
