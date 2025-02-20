a3d4d2638501e66fd5d52c28d7cd90b6b4fba3db
Rafnas-p <rafnasrooni@gmail.com>
2025-02-20 12:41:13 +0530

    recpt

---
+++ src/app/(root)/(user-routes)/user/donationRecipt/page.tsx
@@ -0,0 +1,12 @@
+import Receipt from '@/components/users/donations/racipts'
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+        <Receipt/>
+    </div>
+  )
+}
+
+export default Page
